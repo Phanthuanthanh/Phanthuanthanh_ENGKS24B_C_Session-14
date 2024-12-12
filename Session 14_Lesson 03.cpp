c186ed428bbc0cdbc52aed9f504d4bbc9fe79dcf
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2024-12-12 21:10:26 +0700

    Up load Bai tap Session 14_Lesson 03

---
+++ Session 14_Lesson 03.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char str[] = "Nice to meet you";
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    printf("Chuoi dao nguoc: ");
+    for (int i = length - 1 ; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+    return 0;
+}
